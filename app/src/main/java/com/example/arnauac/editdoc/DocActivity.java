b99a60b875e88fbab2531c098c399211fee49fe0
alloza95 <naldusbros@gmail.com>
2018-10-16 09:21:23 +0200

    Recullim el text a editar a EditActivity

--- app/src/main/java/com/example/arnauac/editdoc/DocActivity.java
+++ app/src/main/java/com/example/arnauac/editdoc/DocActivity.java
@@ -1,5 +1,6 @@
 package com.example.arnauac.editdoc;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -9,6 +10,8 @@ import android.widget.TextView;
 import org.w3c.dom.Text;
 
 public class DocActivity extends AppCompatActivity {
+
+    public static final int EDIT_TITLE = 1;
 
     private String title = "Lorem Ipsu", text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris et purus sed nisl semper tristique a non dolor. Pellentesque tincidunt est leo, sit amet tempus nisi cursus facilisis. Nulla ornare lacinia orci, id pulvinar nunc lobortis ut. Mauris sapien nulla, tincidunt in purus fringilla, posuere hendrerit dui. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Cras non metus et quam dapibus sagittis non tincidunt ante. Aenean lorem risus, euismod et mauris eu, eleifend volutpat ante. Nam non nisl risus. Praesent blandit ligula vitae tellus semper imperdiet. Sed ultrices dolor mauris, at sodales elit mollis nec. Fusce a mi mi. Nullam eget fringilla libero.";
     private TextView title_view;
@@ -36,7 +39,9 @@ public class DocActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()){
             case R.id.option_edit_title:
-                //TODO: Cridar EditActivity
+                Intent intent = new Intent(this, EditActivity.class);
+                intent.putExtra("text", title);
+                startActivityForResult(intent, EDIT_TITLE);
                 break;
         }
         return true;
--- app/src/main/java/com/example/arnauac/editdoc/EditActivity.java
+++ app/src/main/java/com/example/arnauac/editdoc/EditActivity.java
@@ -1,13 +1,27 @@
 package com.example.arnauac.editdoc;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class EditActivity extends AppCompatActivity {
+
+    private EditText edit_box;
+    private Button btn_save;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit);
+
+        Intent intent = getIntent();
+        String text = intent.getStringExtra("text");
+
+        edit_box = findViewById(R.id.editBox);
+        btn_save = findViewById(R.id.btn_Save);
+
+        edit_box.setText(text);
     }
 }
